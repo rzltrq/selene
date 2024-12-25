733c2618e27d17c61d8582fc377fbc4bee049d93
rzltrq <138825322+rzltrq@users.noreply.github.com>
2024-12-26 02:59:58 +0700

    Update main.js

--- main.js
+++ main.js
@@ -61,6 +61,7 @@ function generateBalloons() {
     for (let i = 0; i < 10; i++) {
         createBalloon();
     }
+    
     generateBalloons(); // Start generating balloons
     setInterval(createBalloon, 1000); // Continuously spawn balloons
 
