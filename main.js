7ebbfc2acfb9b224c5ed7dfa3b19e912971047d3
rzltrq <138825322+rzltrq@users.noreply.github.com>
2024-12-26 03:08:59 +0700

    Update main.js

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ function startSlides() {
     audio.play();
     generateBalloons();
     launchFireworks();
+    launchConfetti();
 };
 
 
@@ -100,4 +101,28 @@ function animateFireworks() {
     }
 
     requestAnimationFrame(animateFireworks); // Loop fireworks animation
+}
+
+function launchConfetti() {
+    const duration = 3 * 1000; // Confetti for 3 seconds
+    const end = Date.now() + duration;
+
+    (function frame() {
+        confetti({
+            particleCount: 5,
+            angle: 60,
+            spread: 55,
+            origin: { x: 0 }
+        });
+        confetti({
+            particleCount: 5,
+            angle: 120,
+            spread: 55,
+            origin: { x: 1 }
+        });
+
+        if (Date.now() < end) {
+            requestAnimationFrame(frame);
+        }
+    })();
 }
